5489523beb8cb2ee0d73a65ba702605b16103cad
Alan Maldonado <alan.m@incfile.com>
2026-03-03 12:22:53 -0600

    🚀 Deploy to GH pages

--- src/components/FormField.vue
+++ src/components/FormField.vue
@@ -8,8 +8,10 @@ defineProps({
 
 <template>
   <div class="form-field">
-    <label class="form-field__label">{{ label }}</label>
-    <slot :error="error" />
+    <label class="form-field__label">
+      {{ label }}
+      <slot :error="error" />
+    </label>
     <p v-if="error" class="form-field__error" role="alert" :data-testid="`error-${testId}`">
       {{ error }}
     </p>
